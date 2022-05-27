a70ae04f2a83f96fe94e62e29ceaed680682b909
Arun <rarnkmrbridgelabz.com>
2022-05-27 18:00:30 +0530

    Using Switch case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -6,28 +6,32 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
-//UC-1-attendance-check
+
 		Random random = new Random();
 		int attendance = random.nextInt(3);
 
 		int wagePerHour = 20;
 		int fullDayHours = 8;
-		int partTimeHours=4;
+		int partTimeHours = 4;
 		int wagePerDay = 0;
-
-		if (attendance == 0) 
+//UC-4-using-switch-case		
+		switch (attendance) {
+		case 0:
 			System.out.println("Employee is Absent...");
-//UC-3-calculate-daily-wage-part time
-		else if(attendance==1){
+			break;
+
+		case 1:
 			System.out.println("Employee is Present Part Time...");
 			wagePerDay = wagePerHour * partTimeHours;
-		}
-//UC-2-calculate-daily-wage-full time
-		 else {
+			break;
+
+		case 2:
 			System.out.println("Employee is Present Full Time...");
 			wagePerDay = wagePerHour * fullDayHours;
-		 }
-		System.out.println("Employee Wage Per Day: " + wagePerDay);
-	}
+			break;
+		}
 
+		System.out.println("Employee Wage Per Day: " + wagePerDay);
+
+	}
 }
