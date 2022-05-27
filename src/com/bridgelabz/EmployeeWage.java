b1fa2c08a071c5f1c28f66f5f5dc85c11f08f99d
Arun <rarnkmrbridgelabz.com>
2022-05-27 16:29:10 +0530

    Added:Logic to check attendance

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,9 +1,19 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+//UC-1-attendance -check
+		Random random = new Random();
+		int attendance = random.nextInt(2);
+
+		if (attendance == 1)
+			System.out.println("Employee is Present...");
+		else
+			System.out.println("Employee is Absent...");
 
 	}
 
